95c43db2c748070b5994bb7ff33c2301c09c011d
Ryan White <ryanwhite04@gmail.com>
2022-08-31 00:07:50 +0800

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+function wait
+    while inotifywait "$argv".c;
+        make $argv;
+        ./$argv;
+    end
+end
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end

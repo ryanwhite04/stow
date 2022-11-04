e806362c44b3ae1fdae560a748284af62414024c
Ryan White <ryanwhite04@gmail.com>
2022-11-04 16:32:40 +0800

    Change to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,25 @@
-function wait
-    while inotifywait "$argv".c;
-        make $argv;
-        ./$argv;
-    end
-end
-
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
     if test "$argv" = !!
     eval command sudo $history[1]
 else
     command sudo $argv
     end
+end
+
+function bide
+    argparse 'f/files=' 'c/command=' -- $argv
+    clear;
+    echo "Waiting on $_flag_f";
+    while inotifywait $_flag_f;
+        clear;
+        echo "Running $_flag_c";
+        eval $_flag_c;
+        echo "Waiting on $_flag_f";
+    end
+end
+
+function load
+    set path "$__fish_config_dir/config.fish"
+    echo "Sourcing $path"
+    source $path
 end
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,18 @@
-vim.opt.guicursor = ""
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
+opt = vim.opt
+opt.guicursor = ""
+opt.nu = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+opt.mouse = "a"
+
+g = vim.g
+g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
+g.ale_c_gcc_options = '-std=c11 -Wall -Wextra -DHAVE_CONFIG_H -I. -Wno-pointer-sign'
+g.ale_c_clang_options = '-std=c11 -Wall -Wextra -DHAVE_CONFIG_H -I. -Wno-pointer-sign'
+g.ale_c_clangtidy_checks =  {'-clang-diagnostic-pointer-sign', 'cert-*'}
+g.ale_c_clangtidy_options =  '--extra-arg="-DHAVE_CONFIG_H -I. -Wno-pointer-sign"'
